a73940357fd71c0ad764373c40d1a93d0f9de846
msiglreith <m.siglreith@gmail.com>
2019-10-03 19:02:41 +0200

    Rename XcbHandle surface to window and extend documentation (#28)

--- src/windows.rs
+++ src/windows.rs
@@ -13,6 +13,7 @@ use libc::c_void;
 /// ```
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct WindowsHandle {
+    /// A Win32 HWND handle.
     pub hwnd: *mut c_void,
     #[doc(hidden)]
     #[deprecated = "This field is used to ensure that this struct is non-exhaustive, so that it may be extended in the future. Do not refer to this field."]
