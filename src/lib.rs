5bb1ed099e4181a84bef02e7871d2bd2184edfc8
Lokathor <zefria@gmail.com>
2021-01-25 11:47:29 -0700

    remove cfg guards on the modules and variants.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+#![no_std]
+
 //! Interoperability library for Rust Windowing applications.
 //!
 //! This library provides standard types for accessing a window's platform-specific raw window
@@ -11,76 +13,14 @@
 //! added without breaking backwards compatibility. Each struct provides an `empty` method that may
 //! be used along with the struct update syntax to construct it. See each specific struct for
 //! examples.
-//!
-#![cfg_attr(feature = "nightly-docs", feature(doc_cfg))]
-#![no_std]
 
-#[cfg_attr(feature = "nightly-docs", doc(cfg(target_os = "android")))]
-#[cfg_attr(not(feature = "nightly-docs"), cfg(target_os = "android"))]
 pub mod android;
-#[cfg_attr(feature = "nightly-docs", doc(cfg(target_os = "ios")))]
-#[cfg_attr(not(feature = "nightly-docs"), cfg(target_os = "ios"))]
 pub mod ios;
-#[cfg_attr(feature = "nightly-docs", doc(cfg(target_os = "macos")))]
-#[cfg_attr(not(feature = "nightly-docs"), cfg(target_os = "macos"))]
 pub mod macos;
-#[cfg_attr(feature = "nightly-docs", doc(cfg(target_os = "redox")))]
-#[cfg_attr(not(feature = "nightly-docs"), cfg(target_os = "redox"))]
 pub mod redox;
-#[cfg_attr(
-    feature = "nightly-docs",
-    doc(cfg(any(
-        target_os = "linux",
-        target_os = "dragonfly",
-        target_os = "freebsd",
-        target_os = "netbsd",
-        target_os = "openbsd",
-        target_os = "solaris"
-    )))
-)]
-#[cfg_attr(
-    not(feature = "nightly-docs"),
-    cfg(any(
-        target_os = "linux",
-        target_os = "dragonfly",
-        target_os = "freebsd",
-        target_os = "netbsd",
-        target_os = "openbsd",
-        target_os = "solaris"
-    ))
-)]
 pub mod unix;
-#[cfg_attr(feature = "nightly-docs", doc(cfg(target_arch = "wasm32")))]
-#[cfg_attr(not(feature = "nightly-docs"), cfg(target_arch = "wasm32"))]
 pub mod web;
-#[cfg_attr(feature = "nightly-docs", doc(cfg(target_os = "windows")))]
-#[cfg_attr(not(feature = "nightly-docs"), cfg(target_os = "windows"))]
 pub mod windows;
-
-mod platform {
-    #[cfg(target_os = "android")]
-    pub use crate::android::*;
-    #[cfg(target_os = "macos")]
-    pub use crate::macos::*;
-    #[cfg(target_os = "redox")]
-    pub use crate::redox::*;
-    #[cfg(any(
-        target_os = "linux",
-        target_os = "dragonfly",
-        target_os = "freebsd",
-        target_os = "netbsd",
-        target_os = "openbsd",
-        target_os = "solaris",
-    ))]
-    pub use crate::unix::*;
-    #[cfg(target_os = "windows")]
-    pub use crate::windows::*;
-    // mod platform;
-    #[cfg(target_os = "ios")]
-    pub use crate::ios::*;
-    #[cfg(target_arch = "wasm32")]
-    pub use crate::web::*;
-}
 
 /// Window that wraps around a raw window handle.
 ///
@@ -100,101 +40,26 @@ pub unsafe trait HasRawWindowHandle {
     fn raw_window_handle(&self) -> RawWindowHandle;
 }
 
+/// An enum to simply combine the different possible raw window handle variants.
 #[non_exhaustive]
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub enum RawWindowHandle {
-    #[cfg_attr(feature = "nightly-docs", doc(cfg(target_os = "ios")))]
-    #[cfg_attr(not(feature = "nightly-docs"), cfg(target_os = "ios"))]
     IOS(ios::IOSHandle),
 
-    #[cfg_attr(feature = "nightly-docs", doc(cfg(target_os = "macos")))]
-    #[cfg_attr(not(feature = "nightly-docs"), cfg(target_os = "macos"))]
     MacOS(macos::MacOSHandle),
 
-    #[cfg_attr(feature = "nightly-docs", doc(cfg(target_os = "redox")))]
-    #[cfg_attr(not(feature = "nightly-docs"), cfg(target_os = "redox"))]
     Redox(redox::RedoxHandle),
 
-    #[cfg_attr(
-        feature = "nightly-docs",
-        doc(cfg(any(
-            target_os = "linux",
-            target_os = "dragonfly",
-            target_os = "freebsd",
-            target_os = "netbsd",
-            target_os = "openbsd",
-            target_os = "solaris"
-        )))
-    )]
-    #[cfg_attr(
-        not(feature = "nightly-docs"),
-        cfg(any(
-            target_os = "linux",
-            target_os = "dragonfly",
-            target_os = "freebsd",
-            target_os = "netbsd",
-            target_os = "openbsd",
-            target_os = "solaris"
-        ))
-    )]
     Xlib(unix::XlibHandle),
 
-    #[cfg_attr(
-        feature = "nightly-docs",
-        doc(cfg(any(
-            target_os = "linux",
-            target_os = "dragonfly",
-            target_os = "freebsd",
-            target_os = "netbsd",
-            target_os = "openbsd",
-            target_os = "solaris"
-        )))
-    )]
-    #[cfg_attr(
-        not(feature = "nightly-docs"),
-        cfg(any(
-            target_os = "linux",
-            target_os = "dragonfly",
-            target_os = "freebsd",
-            target_os = "netbsd",
-            target_os = "openbsd",
-            target_os = "solaris"
-        ))
-    )]
     Xcb(unix::XcbHandle),
 
-    #[cfg_attr(
-        feature = "nightly-docs",
-        doc(cfg(any(
-            target_os = "linux",
-            target_os = "dragonfly",
-            target_os = "freebsd",
-            target_os = "netbsd",
-            target_os = "openbsd"
-        )))
-    )]
-    #[cfg_attr(
-        not(feature = "nightly-docs"),
-        cfg(any(
-            target_os = "linux",
-            target_os = "dragonfly",
-            target_os = "freebsd",
-            target_os = "netbsd",
-            target_os = "openbsd"
-        ))
-    )]
     Wayland(unix::WaylandHandle),
 
-    #[cfg_attr(feature = "nightly-docs", doc(cfg(target_os = "windows")))]
-    #[cfg_attr(not(feature = "nightly-docs"), cfg(target_os = "windows"))]
     Windows(windows::WindowsHandle),
 
-    #[cfg_attr(feature = "nightly-docs", doc(cfg(target_arch = "wasm32")))]
-    #[cfg_attr(not(feature = "nightly-docs"), cfg(target_arch = "wasm32"))]
     Web(web::WebHandle),
 
-    #[cfg_attr(feature = "nightly-docs", doc(cfg(target_os = "android")))]
-    #[cfg_attr(not(feature = "nightly-docs"), cfg(target_os = "android"))]
     Android(android::AndroidHandle),
 }
 
