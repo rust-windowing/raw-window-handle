a736d5f6d6d697444c41ad99ab2bc2ef76bf816c
Kirill Chibisov <contact@kchibisov.com>
2022-07-07 02:00:09 +0300

    Decouple display and window from `RawWindowHandle`
    On free platforms there're usually multiple displays, so APIs like
    EGL have a concept of native display when creating EGLDisplay.
    
    Sometimes creating EGLDisplay must be done before creating the window
    on that particular display, to supply e.g. X11 visual information to
    the window that will be created on that display later on.

--- src/unix.rs
+++ src/unix.rs
@@ -1,111 +1,206 @@
 use core::ffi::c_void;
 use core::ptr;
 
-use cty::c_ulong;
+use cty::{c_int, c_ulong};
+
+/// Raw display handle for Xlib.
+///
+/// ## Construction
+/// ```
+/// # use raw_window_handle::XlibDisplayHandle;
+/// let display_handle = XlibDisplayHandle::empty();
+/// /* set fields */
+/// ```
+#[non_exhaustive]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct XlibDisplayHandle {
+    /// A pointer to an Xlib `Display`.
+    pub display: *mut c_void,
+
+    /// An X11 screen to use with this display handle.
+    ///
+    /// Note, that X11 could have multiple screens, however
+    /// graphics APIs could work only with one screen at the time,
+    /// given that multiple screens usually reside on different GPUs.
+    pub screen: c_int,
+}
 
 /// Raw window handle for Xlib.
 ///
 /// ## Construction
 /// ```
-/// # use raw_window_handle::XlibHandle;
-/// let handle = XlibHandle::empty();
+/// # use raw_window_handle::XlibWindowHandle;
+/// let window_handle = XlibWindowHandle::empty();
 /// /* set fields */
 /// ```
 #[non_exhaustive]
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-pub struct XlibHandle {
+pub struct XlibWindowHandle {
     /// An Xlib `Window`.
     pub window: c_ulong,
-    /// A pointer to an Xlib `Display`.
-    pub display: *mut c_void,
     /// An Xlib visual ID, or 0 if unknown.
     pub visual_id: c_ulong,
+}
+
+/// Raw display handle for Xcb.
+///
+/// ## Construction
+/// ```
+/// # use raw_window_handle::XcbDisplayHandle;
+/// let display_handle = XcbDisplayHandle::empty();
+/// /* set fields */
+/// ```
+#[non_exhaustive]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct XcbDisplayHandle {
+    /// A pointer to an X server `xcb_connection_t`.
+    pub connection: *mut c_void,
+
+    /// An X11 screen to use with this display handle.
+    ///
+    /// Note, that X11 could have multiple screens, however
+    /// graphics APIs could work only with one screen at the time,
+    /// given that multiple screens usually reside on different GPUs.
+    pub screen: c_int,
 }
 
 /// Raw window handle for Xcb.
 ///
 /// ## Construction
 /// ```
-/// # use raw_window_handle::XcbHandle;
-/// let handle = XcbHandle::empty();
+/// # use raw_window_handle::XcbWindowHandle;
+/// let window_handle = XcbWindowHandle::empty();
 /// /* set fields */
 /// ```
 #[non_exhaustive]
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-pub struct XcbHandle {
+pub struct XcbWindowHandle {
     /// An X11 `xcb_window_t`.
     pub window: u32, // Based on xproto.h
-    /// A pointer to an X server `xcb_connection_t`.
-    pub connection: *mut c_void,
     /// An X11 `xcb_visualid_t`, or 0 if unknown.
     pub visual_id: u32,
+}
+
+/// Raw display handle for Wayland.
+///
+/// ## Construction
+/// ```
+/// # use raw_window_handle::WaylandDisplayHandle;
+/// let display_handle = WaylandDisplayHandle::empty();
+/// /* set fields */
+/// ```
+#[non_exhaustive]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct WaylandDisplayHandle {
+    /// A pointer to a `wl_display`.
+    pub display: *mut c_void,
 }
 
 /// Raw window handle for Wayland.
 ///
 /// ## Construction
 /// ```
-/// # use raw_window_handle::WaylandHandle;
-/// let handle = WaylandHandle::empty();
+/// # use raw_window_handle::WaylandWindowHandle;
+/// let window_handle = WaylandWindowHandle::empty();
 /// /* set fields */
 /// ```
 #[non_exhaustive]
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-pub struct WaylandHandle {
+pub struct WaylandWindowHandle {
     /// A pointer to a `wl_surface`.
     pub surface: *mut c_void,
-    /// A pointer to a `wl_display`.
-    pub display: *mut c_void,
+}
+
+/// Raw display handle for the Linux Kernel Mode Set/Direct Rendering Manager.
+///
+/// ## Construction
+/// ```
+/// # use raw_window_handle::DrmDisplayHandle;
+/// let display_handle = DrmDisplayHandle::empty();
+/// /* set fields */
+/// ```
+#[non_exhaustive]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct DrmDisplayHandle {
+    /// The drm file descriptor.
+    pub fd: i32,
 }
 
 /// Raw window handle for the Linux Kernel Mode Set/Direct Rendering Manager.
 ///
 /// ## Construction
 /// ```
-/// # use raw_window_handle::DrmHandle;
-/// let handle = DrmHandle::empty();
+/// # use raw_window_handle::DrmWindowHandle;
+/// let handle = DrmWindowHandle::empty();
 /// /* set fields */
 /// ```
 #[non_exhaustive]
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-pub struct DrmHandle {
-    /// The drm file descriptor
-    pub fd: i32,
-    /// The primary drm plane handle
+pub struct DrmWindowHandle {
+    /// The primary drm plane handle.
     pub plane: u32,
 }
 
-impl XlibHandle {
+impl XlibDisplayHandle {
     pub fn empty() -> Self {
         Self {
-            window: 0,
             display: ptr::null_mut(),
-            visual_id: 0,
+            screen: 0,
         }
     }
 }
 
-impl XcbHandle {
+impl XlibWindowHandle {
     pub fn empty() -> Self {
         Self {
             window: 0,
-            connection: ptr::null_mut(),
             visual_id: 0,
         }
     }
 }
 
-impl WaylandHandle {
+impl XcbDisplayHandle {
+    pub fn empty() -> Self {
+        Self {
+            connection: ptr::null_mut(),
+            screen: 0,
+        }
+    }
+}
+
+impl XcbWindowHandle {
+    pub fn empty() -> Self {
+        Self {
+            window: 0,
+            visual_id: 0,
+        }
+    }
+}
+
+impl WaylandDisplayHandle {
+    pub fn empty() -> Self {
+        Self {
+            display: ptr::null_mut(),
+        }
+    }
+}
+
+impl WaylandWindowHandle {
     pub fn empty() -> Self {
         Self {
             surface: ptr::null_mut(),
-            display: ptr::null_mut(),
         }
     }
 }
 
-impl DrmHandle {
+impl DrmDisplayHandle {
     pub fn empty() -> Self {
-        Self { fd: 0, plane: 0 }
+        Self { fd: 0 }
+    }
+}
+
+impl DrmWindowHandle {
+    pub fn empty() -> Self {
+        Self { plane: 0 }
     }
 }
