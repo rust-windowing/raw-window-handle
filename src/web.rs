8c3dfb10fe633757ed6e96ab3ecafed1f931fe47
Mads Marquart <mads@marquart.dk>
2023-07-28 14:42:32 +0200

    Change how handles are constructed, to ensure non-null handles

--- src/web.rs
+++ src/web.rs
@@ -1,29 +1,24 @@
 /// Raw display handle for the Web.
-///
-/// ## Construction
-/// ```
-/// # use raw_window_handle::WebDisplayHandle;
-/// let mut display_handle = WebDisplayHandle::empty();
-/// /* set fields */
-/// ```
 #[non_exhaustive]
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-pub struct WebDisplayHandle;
+pub struct WebDisplayHandle {}
 
 impl WebDisplayHandle {
-    pub fn empty() -> Self {
+    /// Create a new empty display handle.
+    ///
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// # use raw_window_handle::WebDisplayHandle;
+    /// let handle = WebDisplayHandle::new();
+    /// ```
+    pub fn new() -> Self {
         Self {}
     }
 }
 
 /// Raw window handle for the Web.
-///
-/// ## Construction
-/// ```
-/// # use raw_window_handle::WebWindowHandle;
-/// let mut window_handle = WebWindowHandle::empty();
-/// /* set fields */
-/// ```
 #[non_exhaustive]
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub struct WebWindowHandle {
@@ -32,14 +27,24 @@ pub struct WebWindowHandle {
     /// When accessing from JS, the attribute will automatically be called `rawHandle`.
     ///
     /// Each canvas created by the windowing system should be assigned their own unique ID.
-    /// 0 should be reserved for invalid / null IDs.
     ///
     /// [data attributes]: https://developer.mozilla.org/en-US/docs/Web/HTML/Global_attributes/data-*
     pub id: u32,
 }
 
 impl WebWindowHandle {
-    pub fn empty() -> Self {
-        Self { id: 0 }
+    /// Create a new handle to a canvas element.
+    ///
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// # use raw_window_handle::WebWindowHandle;
+    /// #
+    /// let id: u32 = 0; // canvas.rawHandle;
+    /// let handle = WebWindowHandle::new(id);
+    /// ```
+    pub fn new(id: u32) -> Self {
+        Self { id }
     }
 }
