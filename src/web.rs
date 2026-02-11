67ab4063f13e7818602f2768b5efe662c1c19d8a
John Nunley <dev@notgull.net>
2026-02-10 18:33:10 -0800

    breaking: Remove web window handle
    This handle type is no longer used by any windowing systems
    and only exists as a burden for graphics implementors.
    
    Fixes: #157
    Signed-off-by: John Nunley <dev@notgull.net>

--- src/web.rs
+++ src/web.rs
@@ -42,37 +42,6 @@ impl DisplayHandle<'static> {
     }
 }
 
-/// Raw window handle for the Web.
-#[non_exhaustive]
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-pub struct WebWindowHandle {
-    /// An ID value inserted into the [data attributes] of the canvas element as '`raw-handle`'.
-    ///
-    /// When accessing from JS, the attribute will automatically be called `rawHandle`.
-    ///
-    /// Each canvas created by the windowing system should be assigned their own unique ID.
-    ///
-    /// [data attributes]: https://developer.mozilla.org/en-US/docs/Web/HTML/Global_attributes/data-*
-    pub id: u32,
-}
-
-impl WebWindowHandle {
-    /// Create a new handle to a canvas element.
-    ///
-    ///
-    /// # Example
-    ///
-    /// ```
-    /// # use raw_window_handle::WebWindowHandle;
-    /// #
-    /// let id: u32 = 0; // canvas.rawHandle;
-    /// let handle = WebWindowHandle::new(id);
-    /// ```
-    pub fn new(id: u32) -> Self {
-        Self { id }
-    }
-}
-
 /// Raw window handle for a Web canvas registered via [`wasm-bindgen`].
 ///
 /// [`wasm-bindgen`]: https://crates.io/crates/wasm-bindgen
