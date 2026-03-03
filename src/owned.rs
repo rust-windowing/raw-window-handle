0add7915f9e5121dac2dbdd30937fc3da744f758
John Nunley <dev@notgull.net>
2026-03-02 22:17:01 -0800

    feat: Add safe ways to construct owned handles
    This adds two ways to construct owned handles:
    
    - Using an `&'static impl AsWindowHandle`. This is simple because
      `retain` and `release` can be implemented as no-ops. This exists to
      make the case of a singleton no-op display simpler. Each handle
      provides a `WindowHandle::unavailable()` function to demonstrate this
      in action.
    - Using a reference counted container. `WindowHandle` allows `Arc<T>`.
      This allows window handles to be constructed from ref-counted
      containers quite easily.
    
    Both of these strategies allow for window handles building on existing
    implementations to be implemented in entirely safe code.
    
    Signed-off-by: John Nunley <dev@notgull.net>

--- src/owned.rs
+++ src/owned.rs
@@ -18,6 +18,21 @@ pub struct WindowHandle {
 unsafe impl Send for WindowHandle {}
 unsafe impl Sync for WindowHandle {}
 
+macro_rules! noop_window_handle {
+    ($e:expr) => {{
+        struct Noop;
+
+        impl AsWindowHandle for Noop {
+            #[inline(always)]
+            fn window_handle(&self) -> Result<BorrowedWindowHandle<'_>, HandleError> {
+                $e
+            }
+        }
+
+        WindowHandle::from_static(&Noop)
+    }};
+}
+
 impl WindowHandle {
     /// Create a new window handle from its raw parts.
     ///
@@ -45,6 +60,87 @@ impl WindowHandle {
             let new_ptr = (self.vtable.retain)(self.ptr)?;
             Self::from_raw_parts(new_ptr, self.vtable)
         })
+    }
+
+    /// Create a `WindowHandle` from a static window handle reference.
+    ///
+    /// ## Example
+    ///
+    /// ```
+    /// use raw_window_handle::{WindowHandle, XcbWindowHandle, BorrowedWindowHandle, AsWindowHandle, HandleError};
+    ///
+    /// struct MyX11Window(u32);
+    ///
+    /// impl AsWindowHandle for MyX11Window {
+    ///     #[inline]
+    ///     fn window_handle(&self) -> Result<BorrowedWindowHandle<'_>, HandleError> {
+    ///         Ok(unsafe { BorrowedWindowHandle::borrow_raw(XcbWindowHandle::new(self.0.try_into().unwrap()).into()) })
+    ///     }
+    /// }
+    ///
+    /// static WINDOW: MyX11Window = MyX11Window(67);
+    /// let handle = WindowHandle::from_static(&WINDOW);
+    /// match handle.window_handle().unwrap().as_raw() {
+    ///     raw_window_handle::RawWindowHandle::Xcb(xcb) => assert_eq!(xcb.window.get(), 67),
+    ///     _ => unreachable!()
+    /// }
+    /// ```
+    #[inline]
+    pub fn from_static<T: AsWindowHandle + Sync + 'static>(value: &'static T) -> Self {
+        struct Static<T>(T);
+
+        impl<T: AsWindowHandle + Sync + 'static> Static<T> {
+            const VTABLE: WindowVtable = WindowVtable::new(Self::retain, Self::release, Self::get);
+
+            #[inline(always)]
+            unsafe fn retain(value: NonNull<()>) -> Result<NonNull<()>, HandleError> {
+                Ok(value)
+            }
+            #[inline(always)]
+            unsafe fn release(_: NonNull<()>) {}
+            #[inline(always)]
+            unsafe fn get(value: NonNull<()>) -> Result<RawWindowHandle, HandleError> {
+                let value: &'static T = unsafe { value.cast().as_ref() };
+                value.window_handle().map(|h| h.as_raw())
+            }
+        }
+
+        unsafe {
+            WindowHandle::from_raw_parts(
+                NonNull::new_unchecked(value as *const T as *const () as *mut ()),
+                &Static::<T>::VTABLE,
+            )
+        }
+    }
+
+    /// Create a [`WindowHandle`] that always returns `HandleError::Unavailable`.
+    ///
+    /// ## Example
+    ///
+    /// ```
+    /// use raw_window_handle::{WindowHandle, HandleError, AsWindowHandle};
+    ///
+    /// let handle = WindowHandle::unavailable();
+    /// assert!(matches!(handle.window_handle().unwrap_err(), HandleError::Unavailable));
+    /// ```
+    #[inline]
+    pub fn unavailable() -> Self {
+        noop_window_handle!(Err(HandleError::Unavailable))
+    }
+
+    /// Create a [`WindowHandle`] that always returns `HandleError::NotSupported`.
+    ///
+    /// ## Example
+    ///
+    /// ```
+    /// use raw_window_handle::{WindowHandle, HandleError, AsWindowHandle};
+    ///
+    /// let handle = WindowHandle::not_supported();
+    /// assert!(matches!(handle.window_handle().unwrap_err(), HandleError::NotSupported));
+    /// ```
+    #[inline]
+    pub fn not_supported() -> Self {
+        noop_window_handle!(Err(HandleError::NotSupported))
     }
 }
 
@@ -77,6 +173,74 @@ impl AsWindowHandle for WindowHandle {
     }
 }
 
+#[cfg(feature = "alloc")]
+impl<T: AsWindowHandle + Send + Sync + 'static> From<alloc::sync::Arc<T>> for WindowHandle {
+    /// Create a `SyncWindowHandle` from a reference counted underlying handle.
+    ///
+    /// ## Example
+    ///
+    /// ```
+    /// use raw_window_handle::{WindowHandle, XcbWindowHandle, BorrowedWindowHandle, AsWindowHandle, HandleError};
+    /// use std::sync::Arc;
+    ///
+    /// struct MyX11Window(u32);
+    ///
+    /// impl AsWindowHandle for MyX11Window {
+    ///     #[inline]
+    ///     fn window_handle(&self) -> Result<BorrowedWindowHandle<'_>, HandleError> {
+    ///         Ok(unsafe { BorrowedWindowHandle::borrow_raw(XcbWindowHandle::new(self.0.try_into().unwrap()).into()) })
+    ///     }
+    /// }
+    ///
+    /// let window = Arc::new(MyX11Window(67));
+    /// let handle = WindowHandle::from(window);
+    /// match handle.window_handle().unwrap().as_raw() {
+    ///     raw_window_handle::RawWindowHandle::Xcb(xcb) => assert_eq!(xcb.window.get(), 67),
+    ///     _ => unreachable!()
+    /// }
+    /// ```
+    #[inline]
+    fn from(value: alloc::sync::Arc<T>) -> Self {
+        struct Arc<T>(T);
+
+        impl<T: AsWindowHandle + Send + Sync + 'static> Arc<T> {
+            const VTABLE: WindowVtable = WindowVtable::new(Self::retain, Self::release, Self::get);
+
+            #[inline(always)]
+            unsafe fn retain(ptr: NonNull<()>) -> Result<NonNull<()>, HandleError> {
+                // Increment the refcount.
+                unsafe {
+                    alloc::sync::Arc::increment_strong_count(ptr.as_ptr().cast::<T>());
+                }
+
+                Ok(ptr)
+            }
+
+            #[inline(always)]
+            unsafe fn release(ptr: NonNull<()>) {
+                // Decrement the refcount.
+                unsafe {
+                    alloc::sync::Arc::decrement_strong_count(ptr.as_ptr().cast::<T>());
+                }
+            }
+
+            #[inline(always)]
+            unsafe fn get(ptr: NonNull<()>) -> Result<RawWindowHandle, HandleError> {
+                let value: &T = unsafe { ptr.cast().as_ref() };
+                value.window_handle().map(|h| h.as_raw())
+            }
+        }
+
+        // SAFETY: Above VTable is valid for this Rc.
+        unsafe {
+            WindowHandle::from_raw_parts(
+                NonNull::new_unchecked(alloc::sync::Arc::into_raw(value).cast::<()>() as *mut ()),
+                &Arc::<T>::VTABLE,
+            )
+        }
+    }
+}
+
 /// Virtual function table for [`WindowHandle`].
 pub struct WindowVtable {
     /// Increment the refcount of the window handle.
@@ -85,7 +249,7 @@ pub struct WindowVtable {
     /// Decrement the refcount of the window handle.
     ///
     /// Errors in the underlying release are expected to be ignored.
-    release: unsafe fn(NonNull<()>) -> NonNull<()>,
+    release: unsafe fn(NonNull<()>),
 
     /// Get the underlying window handle.
     get: unsafe fn(NonNull<()>) -> Result<RawWindowHandle, HandleError>,
@@ -96,7 +260,7 @@ impl WindowVtable {
     #[inline]
     pub const fn new(
         retain: unsafe fn(NonNull<()>) -> Result<NonNull<()>, HandleError>,
-        release: unsafe fn(NonNull<()>) -> NonNull<()>,
+        release: unsafe fn(NonNull<()>),
         get: unsafe fn(NonNull<()>) -> Result<RawWindowHandle, HandleError>,
     ) -> Self {
         Self {
@@ -118,6 +282,21 @@ pub struct DisplayHandle {
 
 unsafe impl Send for DisplayHandle {}
 unsafe impl Sync for DisplayHandle {}
+
+macro_rules! noop_display_handle {
+    ($e:expr) => {{
+        struct Noop;
+
+        impl AsDisplayHandle for Noop {
+            #[inline(always)]
+            fn display_handle(&self) -> Result<BorrowedDisplayHandle<'_>, HandleError> {
+                $e
+            }
+        }
+
+        DisplayHandle::from_static(&Noop)
+    }};
+}
 
 impl DisplayHandle {
     /// Create a new Display handle from its raw parts.
@@ -146,6 +325,88 @@ impl DisplayHandle {
             let new_ptr = (self.vtable.retain)(self.ptr)?;
             Self::from_raw_parts(new_ptr, self.vtable)
         })
+    }
+
+    /// Create a `DisplayHandle` from a static Display handle reference.
+    ///
+    /// ## Example
+    ///
+    /// ```
+    /// use raw_window_handle::{DisplayHandle, BorrowedDisplayHandle, AsDisplayHandle, HandleError};
+    ///
+    /// struct MyDisplay;
+    ///
+    /// impl AsDisplayHandle for MyDisplay {
+    ///     #[inline]
+    ///     fn display_handle(&self) -> Result<BorrowedDisplayHandle<'_>, HandleError> {
+    ///         Ok(BorrowedDisplayHandle::windows())
+    ///     }
+    /// }
+    ///
+    /// static DISPLAY: MyDisplay = MyDisplay;
+    /// let handle = DisplayHandle::from_static(&DISPLAY);
+    /// match handle.display_handle().unwrap().as_raw() {
+    ///     raw_window_handle::RawDisplayHandle::Windows(_) => {}
+    ///     _ => unreachable!()
+    /// }
+    /// ```
+    #[inline]
+    pub fn from_static<T: AsDisplayHandle + Sync + 'static>(value: &'static T) -> Self {
+        struct Static<T>(T);
+
+        impl<T: AsDisplayHandle + Sync + 'static> Static<T> {
+            const VTABLE: DisplayVtable =
+                DisplayVtable::new(Self::retain, Self::release, Self::get);
+
+            #[inline(always)]
+            unsafe fn retain(value: NonNull<()>) -> Result<NonNull<()>, HandleError> {
+                Ok(value)
+            }
+            #[inline(always)]
+            unsafe fn release(_: NonNull<()>) {}
+            #[inline(always)]
+            unsafe fn get(value: NonNull<()>) -> Result<RawDisplayHandle, HandleError> {
+                let value: &'static T = unsafe { value.cast().as_ref() };
+                value.display_handle().map(|h| h.as_raw())
+            }
+        }
+
+        unsafe {
+            DisplayHandle::from_raw_parts(
+                NonNull::new_unchecked(value as *const T as *const () as *mut ()),
+                &Static::<T>::VTABLE,
+            )
+        }
+    }
+
+    /// Create a [`DisplayHandle`] that always returns `HandleError::Unavailable`.
+    ///
+    /// ## Example
+    ///
+    /// ```
+    /// use raw_window_handle::{DisplayHandle, HandleError, AsDisplayHandle};
+    ///
+    /// let handle = DisplayHandle::unavailable();
+    /// assert!(matches!(handle.display_handle().unwrap_err(), HandleError::Unavailable));
+    /// ```
+    #[inline]
+    pub fn unavailable() -> Self {
+        noop_display_handle!(Err(HandleError::Unavailable))
+    }
+
+    /// Create a [`DisplayHandle`] that always returns `HandleError::NotSupported`.
+    ///
+    /// ## Example
+    ///
+    /// ```
+    /// use raw_window_handle::{DisplayHandle, HandleError, AsDisplayHandle};
+    ///
+    /// let handle = DisplayHandle::not_supported();
+    /// assert!(matches!(handle.display_handle().unwrap_err(), HandleError::NotSupported));
+    /// ```
+    #[inline]
+    pub fn not_supported() -> Self {
+        noop_display_handle!(Err(HandleError::NotSupported))
     }
 }
 
@@ -178,6 +439,75 @@ impl AsDisplayHandle for DisplayHandle {
     }
 }
 
+#[cfg(feature = "alloc")]
+impl<T: AsDisplayHandle + Send + Sync + 'static> From<alloc::sync::Arc<T>> for DisplayHandle {
+    /// Create a `DisplayHandle` from a reference counted underlying handle.
+    ///
+    /// ## Example
+    ///
+    /// ```
+    /// use raw_window_handle::{DisplayHandle, BorrowedDisplayHandle, AsDisplayHandle, HandleError};
+    /// use std::sync::Arc;
+    ///
+    /// struct MyDisplay;
+    ///
+    /// impl AsDisplayHandle for MyDisplay {
+    ///     #[inline]
+    ///     fn display_handle(&self) -> Result<BorrowedDisplayHandle<'_>, HandleError> {
+    ///         Ok(BorrowedDisplayHandle::windows())
+    ///     }
+    /// }
+    ///
+    /// let display = Arc::new(MyDisplay);
+    /// let handle = DisplayHandle::from(display);
+    /// match handle.display_handle().unwrap().as_raw() {
+    ///     raw_window_handle::RawDisplayHandle::Windows(_) => {},
+    ///     _ => unreachable!()
+    /// }
+    /// ```
+    #[inline]
+    fn from(value: alloc::sync::Arc<T>) -> Self {
+        struct Arc<T>(T);
+
+        impl<T: AsDisplayHandle + Send + Sync + 'static> Arc<T> {
+            const VTABLE: DisplayVtable =
+                DisplayVtable::new(Self::retain, Self::release, Self::get);
+
+            #[inline(always)]
+            unsafe fn retain(ptr: NonNull<()>) -> Result<NonNull<()>, HandleError> {
+                // Increment the refcount.
+                unsafe {
+                    alloc::sync::Arc::increment_strong_count(ptr.as_ptr().cast::<T>());
+                }
+
+                Ok(ptr)
+            }
+
+            #[inline(always)]
+            unsafe fn release(ptr: NonNull<()>) {
+                // Decrement the refcount.
+                unsafe {
+                    alloc::sync::Arc::decrement_strong_count(ptr.as_ptr().cast::<T>());
+                }
+            }
+
+            #[inline(always)]
+            unsafe fn get(ptr: NonNull<()>) -> Result<RawDisplayHandle, HandleError> {
+                let value: &T = unsafe { ptr.cast().as_ref() };
+                value.display_handle().map(|h| h.as_raw())
+            }
+        }
+
+        // SAFETY: Above VTable is valid for this Rc.
+        unsafe {
+            DisplayHandle::from_raw_parts(
+                NonNull::new_unchecked(alloc::sync::Arc::into_raw(value).cast::<()>() as *mut ()),
+                &Arc::<T>::VTABLE,
+            )
+        }
+    }
+}
+
 /// Virtual function table for [`DisplayHandle`].
 pub struct DisplayVtable {
     /// Increment the refcount of the Display handle.
@@ -186,7 +516,7 @@ pub struct DisplayVtable {
     /// Decrement the refcount of the Display handle.
     ///
     /// Errors in the underlying release are expected to be ignored.
-    release: unsafe fn(NonNull<()>) -> NonNull<()>,
+    release: unsafe fn(NonNull<()>),
 
     /// Get the underlying Display handle.
     get: unsafe fn(NonNull<()>) -> Result<RawDisplayHandle, HandleError>,
@@ -197,7 +527,7 @@ impl DisplayVtable {
     #[inline]
     pub const fn new(
         retain: unsafe fn(NonNull<()>) -> Result<NonNull<()>, HandleError>,
-        release: unsafe fn(NonNull<()>) -> NonNull<()>,
+        release: unsafe fn(NonNull<()>),
         get: unsafe fn(NonNull<()>) -> Result<RawDisplayHandle, HandleError>,
     ) -> Self {
         Self {
