d7b7e1d56252067416e4e7709eb3ac7bb35975ea
Mads Marquart <mads@marquart.dk>
2026-03-16 04:42:19 +0100

    Rename DisplayHandle::web -> DisplayHandle::wasm_bindgen
    Small oversight from be804e9.

--- src/web.rs
+++ src/web.rs
@@ -41,7 +41,7 @@ impl WasmBindgenDisplay {
 }
 
 impl DisplayHandle<'static> {
-    /// Create a Web-based display handle.
+    /// Create a `wasm-bindgen`-based display handle.
     ///
     /// As no data is borrowed by this handle, it is completely safe to create. This function
     /// may be useful to windowing framework implementations that want to avoid unsafe code.
@@ -51,10 +51,10 @@ impl DisplayHandle<'static> {
     /// ```
     /// # use raw_window_handle::{DisplayHandle, HasDisplayHandle};
     /// # fn do_something(rwh: impl HasDisplayHandle) { let _ = rwh; }
-    /// let handle = DisplayHandle::web();
+    /// let handle = DisplayHandle::wasm_bindgen();
     /// do_something(handle);
     /// ```
-    pub fn web() -> Self {
+    pub fn wasm_bindgen() -> Self {
         // SAFETY: No data is borrowed.
         unsafe { Self::borrow_raw(WasmBindgenDisplay::new().into()) }
     }
